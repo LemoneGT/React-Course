d636569c2512ba4a0208515eaff98934d2df3c86
TheMiguelGt <miguel.74ngel@gmail.com>
2024-04-28 22:25:23 -0600

    create a new component

---
+++ Course/03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,5 @@
+export const FirstApp = () => {
+    return(
+        <h1>Soy un tiburon</h1>
+    )
+}
--- Course/03-counter-app-vite/src/main.jsx
+++ Course/03-counter-app-vite/src/main.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import {HelloWorldApp} from './HelloWorldApp';
+// import {HelloWorldApp} from './HelloWorldApp';
+import { FirstApp } from './FirstApp';
 
 // import HelloWOrldApp from './HelloWorldApp'
 
 
 // you nedd to add the name of your function to agregate in react en generate the object
-ReactDOM.createRoot( document.getElementById('root')).render(
+// ReactDOM.createRoot( document.getElementById('root')).render(
+//     <React.StrictMode>
+//         <HelloWorldApp />
+//     </React.StrictMode>
+// )
+
+
+ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <HelloWorldApp />
+        <FirstApp/>
     </React.StrictMode>
 )

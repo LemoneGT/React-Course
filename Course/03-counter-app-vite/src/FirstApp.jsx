1d054e3ddd097a98181163742558f585155231f8
TheMiguelGt <miguel.74ngel@gmail.com>
2024-04-28 22:36:32 -0600

    add more elements in your component

--- Course/03-counter-app-vite/src/FirstApp.jsx
+++ Course/03-counter-app-vite/src/FirstApp.jsx
@@ -1,5 +1,22 @@
+import { Fragment } from 'react';
+
+
+// use fragment to add two elementes or components in component
+// export const FirstApp = () => {
+//     return(
+//         <Fragment>
+//             <h1>Soy un tiburon</h1>
+//             <p>Subtitulo</p>
+//         </Fragment>
+//     )
+// }
+
+// if you dont write nothing in EventTarget, is the same to fragment
 export const FirstApp = () => {
     return(
-        <h1>Soy un tiburon</h1>
+        <>
+            <h1>Soy un tiburon</h1>
+            <p>Subtitulo</p>
+        </>
     )
 }

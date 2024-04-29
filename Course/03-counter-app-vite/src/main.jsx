8f2d670b614514774dc58d42f3eeb241ffb62c25
TheMiguelGt <miguel.74ngel@gmail.com>
2024-04-28 22:05:59 -0600

    create hello world in main

--- Course/03-counter-app-vite/src/main.jsx
+++ Course/03-counter-app-vite/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+function App(){
+    // document.createELement...
+    return  (<h1>Hola mundo!!</h1>);
+}
+
+// you nedd to add the name of your function to agregate in react en generate the object
+ReactDOM.createRoot( document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )

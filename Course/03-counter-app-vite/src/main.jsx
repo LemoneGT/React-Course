9acaa35e0d9bda0a86a652c8bf33be919914e2a8
TheMiguelGt <miguel.74ngel@gmail.com>
2024-04-28 22:18:12 -0600

    my first functional component

--- Course/03-counter-app-vite/src/main.jsx
+++ Course/03-counter-app-vite/src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
+import {HelloWorldApp} from './HelloWorldApp';
 
-function App(){
-    // document.createELement...
-    return  (<h1>Hola mundo!!</h1>);
-}
+// import HelloWOrldApp from './HelloWorldApp'
+
 
 // you nedd to add the name of your function to agregate in react en generate the object
 ReactDOM.createRoot( document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <HelloWorldApp />
     </React.StrictMode>
 )
